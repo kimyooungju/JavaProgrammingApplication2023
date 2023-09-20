b21c236cb7f2763629b5d2f40e41d8e64572e001
Kimyooungju <2020136058@g.shingu.ac.kr>
2023-09-20 15:00:49 +0900

    week04 v20

--- week04/src/PokemonGame.java
+++ week04/src/PokemonGame.java
@@ -1,10 +1,26 @@
+import java.sql.SQLOutput;
 
 public class PokemonGame {
     public static void main(String[] args) {
-        NoFly noFly = new NoFly();
-        Pikachu pikachu = new Pikachu(noFly);  // Association 연관관계, Has-a (Aggregation) //pikachu 객체가 없어져도 nofly객체 다른객체에 사용가능
-       // pikachu.setFlyable(new NoFly()); // 프로그램 실행 중 동적으로 하늘을 나는 방식 변경
+        System.out.println("포켓몬 게임을 시작합니다/n야생 포켓몬이 나타낫습니다");
+      //  System.out.println(Math.random());   //범위 0.0 <= x < 1.0
+      //  System.out.println((int)(Math.random() * 6)+1);   //범위 1 <= x < 6
+        
+        //적군 랜덤 생성 코드
+        int enemyPick = (int)(Math.random() * 3);
+        System.out.println(enemyPick);
+        if(enemyPick == 0){
+            NoFly noFly = new NoFly();
+            Pikachu pikachu = new Pikachu(noFly);
+        }else if(enemyPick == 1){
+            NoFly noFly = new NoFly();
+            Squirtle pikachu = new Squirtle(noFly);
+        }else if(enemyPick == 2){
+            Wings wings = new Wings();
+            Charizard pikachu = new Charizard(wings);
+        }else{
+            System.out.println("여기는 영원히 실행이 안됩니다");
+        }
 
-        pikachu.performFly();
     }
 }
