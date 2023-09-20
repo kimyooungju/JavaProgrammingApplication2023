bd5d45b9a5c6a895e7de1a863e5850e621c244d5
Kimyooungju <2020136058@g.shingu.ac.kr>
2023-09-20 15:49:07 +0900

    week04 v22

--- week04/src/PokemonGame.java
+++ week04/src/PokemonGame.java
@@ -8,31 +8,34 @@ public class PokemonGame {
       //  System.out.println((int)(Math.random() * 6)+1);   //범위 1 <= x < 6
         
         //적군 랜덤 생성 코드
+        Pokemon enemy = null;
         int enemyPick = (int)(Math.random() * 3);
         System.out.println(enemyPick);
         if(enemyPick == 0){
             NoFly noFly = new NoFly();
-            Pikachu pikachu = new Pikachu(noFly);
+            enemy = new Pikachu(noFly);
         }else if(enemyPick == 1){
             NoFly noFly = new NoFly();
-            Squirtle squirtle = new Squirtle(noFly);
+            enemy = new Squirtle(noFly);
         }else if(enemyPick == 2){
             Wings wings = new Wings();
-            Charizard charizard = new Charizard(wings);
+            enemy = new Charizard(wings);
         }else{
             System.out.println("여기는 영원히 실행이 안됩니다");
         }
 
         //플레이어 포켓몬스터 선택
+        //Pokemon player = new Pokemon(); //추상클래스는 객체 생성 불가
+        Pokemon player = null; //추상클래스의 변수 선언은 가능 (upcasting 용)
         Scanner scanner = new Scanner(System.in);
         System.out.println("포켓몬을 고르세요.\n1) 피카츄   2) 꼬부기   3)리자몽 :");
         int pokemonPick = scanner.nextInt();
         if(pokemonPick == 1){
-            Pikachu p1 = new Pikachu(new NoFly());
+            player = new Pikachu(new NoFly());
         }else if(pokemonPick == 2){
-            Squirtle p2 = new Squirtle(new NoFly());
+            player = new Squirtle(new NoFly());
         }else if(pokemonPick == 3){
-            Charizard p3 = new Charizard(new NoFly());
+            player = new Charizard(new NoFly());
         }else {
             System.out.println("정상적인 값이 아닙니다");
         }
@@ -41,7 +44,7 @@ public class PokemonGame {
             System.out.println("\t1 전투   2) 도망   3) 종료 :");
             int menu = scanner.nextInt();
             if(menu == 1){
-                
+                player.attack(enemy);
             }else if(menu == 2){
 
             }else if(menu == 3){
